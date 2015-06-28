a23cc4fa902c1a0e9f7c59e01370f2b6053d6e78
Shunfan Du <i@perry.asia>
2015-06-28 20:41:02 +0800

    Rewrite useless codes.

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,74 +1,92 @@
 var tibet = document.getElementById('tibet-gallery');
+
+if (tibet != undefined) {
+  $('#tibet-gallery').hide();
+
+  // Tibet Handler
+  var tibet_vertical_images = [1, 2, 5, 6, 9, 10, 11, 16, 46, 50, 51, 54, 57];
+  for(var i = 1; i < 70; i++) {
+    if(tibet_vertical_images.indexOf(i) >= 0) {
+      tibet.insertAdjacentHTML('beforeend', '<figure class="item size23" itemprop="associatedMedia"' +
+          'itemscope itemtype="http://schema.org/ImageObject">' +
+          '<a href="https://s3-us-west-1.amazonaws.com/perry-asia/tibet/image' + i +
+          '.jpg" itemprop="contentUrl" data-size="720x1080">' +
+          '<img src="https://s3-us-west-1.amazonaws.com/perry-asia/tibet/thumbnail' + i +
+          '.jpg" itemprop="thumbnail" /></a></figure></div>')
+    } else {
+      tibet.insertAdjacentHTML('beforeend', '<figure class="item size32" itemprop="associatedMedia"' +
+          'itemscope itemtype="http://schema.org/ImageObject">' +
+          '<a href="https://s3-us-west-1.amazonaws.com/perry-asia/tibet/image' + i +
+          '.jpg" itemprop="contentUrl" data-size="1620x1080">' +
+          '<img src="https://s3-us-west-1.amazonaws.com/perry-asia/tibet/thumbnail' + i +
+          '.jpg" itemprop="thumbnail" /></a></figure>')
+    }
+  }
+
+  $('.loading').html("Loading...");
+
+  // After all the images are loaded
+  $(window).load(function(){
+    // Remove the loading bar
+    $('.loading').remove();
+
+    // Show the gallery now
+    $('#tibet-gallery').show();
+
+    var tibet_container = document.querySelector('#tibet-gallery');
+
+    var pckry = new Packery(tibet_container, {
+      // options
+      itemSelector: '.item',
+    });
+  });
+}
+
 var nepal = document.getElementById('nepal-gallery');
 
-// Hide the gallery first
-$('#tibet-gallery').hide();
-$('#nepal-gallery').hide();
+if (nepal != undefined) {
+  // Hide the gallery first
+  $('#nepal-gallery').hide();
 
-// Tibet Handler
-var tibet_vertical_images = [1, 2, 5, 6, 9, 10, 11, 16, 46, 50, 51, 54, 57];
-for(var i = 1; i < 70; i++) {
-  if(tibet_vertical_images.indexOf(i) >= 0) {
-    tibet.insertAdjacentHTML('beforeend', '<figure class="item size23" itemprop="associatedMedia"' +
-      'itemscope itemtype="http://schema.org/ImageObject">' +
-      '<a href="https://s3-us-west-1.amazonaws.com/perry-asia/tibet/image' + i +
-      '.jpg" itemprop="contentUrl" data-size="720x1080">' +
-      '<img src="https://s3-us-west-1.amazonaws.com/perry-asia/tibet/thumbnail' + i +
-      '.jpg" itemprop="thumbnail" /></a></figure></div>')
-  } else {
-    tibet.insertAdjacentHTML('beforeend', '<figure class="item size32" itemprop="associatedMedia"' +
-      'itemscope itemtype="http://schema.org/ImageObject">' +
-      '<a href="https://s3-us-west-1.amazonaws.com/perry-asia/tibet/image' + i +
-      '.jpg" itemprop="contentUrl" data-size="1620x1080">' +
-      '<img src="https://s3-us-west-1.amazonaws.com/perry-asia/tibet/thumbnail' + i +
-      '.jpg" itemprop="thumbnail" /></a></figure>')
+  // Nepal Handler
+  var nepal_vertical_images = [3, 5, 10, 11, 15, 26, 35, 43, 44, 46, 48, 50, 52, 55, 56, 59, 61, 69, 74]
+  for(var i = 1; i < 89; i++) {
+    if(nepal_vertical_images.indexOf(i) >= 0) {
+      nepal.insertAdjacentHTML('beforeend', '<figure class="item size23" itemprop="associatedMedia"' +
+          'itemscope itemtype="http://schema.org/ImageObject">' +
+          '<a href="https://s3-us-west-1.amazonaws.com/perry-asia/nepal/image' + i +
+          '.jpg" itemprop="contentUrl" data-size="720x1080">' +
+          '<img src="https://s3-us-west-1.amazonaws.com/perry-asia/nepal/thumbnail' + i +
+          '.jpg" itemprop="thumbnail" /></a></figure></div>')
+    } else {
+      nepal.insertAdjacentHTML('beforeend', '<figure class="item size32" itemprop="associatedMedia"' +
+          'itemscope itemtype="http://schema.org/ImageObject">' +
+          '<a href="https://s3-us-west-1.amazonaws.com/perry-asia/nepal/image' + i +
+          '.jpg" itemprop="contentUrl" data-size="1620x1080">' +
+          '<img src="https://s3-us-west-1.amazonaws.com/perry-asia/nepal/thumbnail' + i +
+          '.jpg" itemprop="thumbnail" /></a></figure>')
+    }
   }
-}
 
-// Nepal Handler
-var nepal_vertical_images = [3, 5, 10, 11, 15, 26, 35, 43, 44, 46, 48, 50, 52, 55, 56, 59, 61, 69, 74]
-for(var i = 1; i < 89; i++) {
-  if(nepal_vertical_images.indexOf(i) >= 0) {
-    nepal.insertAdjacentHTML('beforeend', '<figure class="item size23" itemprop="associatedMedia"' +
-        'itemscope itemtype="http://schema.org/ImageObject">' +
-        '<a href="https://s3-us-west-1.amazonaws.com/perry-asia/nepal/image' + i +
-        '.jpg" itemprop="contentUrl" data-size="720x1080">' +
-        '<img src="https://s3-us-west-1.amazonaws.com/perry-asia/nepal/thumbnail' + i +
-        '.jpg" itemprop="thumbnail" /></a></figure></div>')
-  } else {
-    nepal.insertAdjacentHTML('beforeend', '<figure class="item size32" itemprop="associatedMedia"' +
-        'itemscope itemtype="http://schema.org/ImageObject">' +
-        '<a href="https://s3-us-west-1.amazonaws.com/perry-asia/nepal/image' + i +
-        '.jpg" itemprop="contentUrl" data-size="1620x1080">' +
-        '<img src="https://s3-us-west-1.amazonaws.com/perry-asia/nepal/thumbnail' + i +
-        '.jpg" itemprop="thumbnail" /></a></figure>')
-  }
-}
+  $('.loading').html("Loading...");
 
-$('.loading').html("Loading...");
+  // After all the images are loaded
+  $(window).load(function(){
+    // Remove the loading bar
+    $('.loading').remove();
 
-// After all the images are loaded
-$(window).load(function(){
+    // Show the gallery now
+    $('#tibet-gallery').show();
+    $('#nepal-gallery').show();
 
-  // Remove the loading bar
-  $('.loading').remove();
+    var nepal_container = document.querySelector('#nepal-gallery');
 
-  // Show the gallery now
-  $('#tibet-gallery').show();
-  $('#nepal-gallery').show();
-
-  var tibet_container = document.querySelector('#tibet-gallery');
-  var nepal_container = document.querySelector('#nepal-gallery');
-
-  var pckry = new Packery(tibet_container, {
-    // options
-    itemSelector: '.item',
+    var pckry = new Packery(nepal_container, {
+      // options
+      itemSelector: '.item',
+    });
   });
-  var pckry = new Packery(nepal_container, {
-    // options
-    itemSelector: '.item',
-  });
-});
+}
 
 var initPhotoSwipeFromDOM = function(gallerySelector) {
 
